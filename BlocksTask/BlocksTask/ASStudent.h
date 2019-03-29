bfc808e736d72139ebe6feb60922753c4e76b308
Aleksandr <djturbo9393@gmail.com>
2019-03-30 00:10:53 +0300

    Добавлено задание по блокам

---
+++ BlocksTask/BlocksTask/ASStudent.h
@@ -0,0 +1,20 @@
+//
+//  ASStudent.h
+//  BlocksTask
+//
+//  Created by Aleksandr on 29/03/2019.
+//  Copyright © 2019 Aleksandr Shushkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ASStudent : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *lastName;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BlocksTask/BlocksTask/ASStudent.m
@@ -0,0 +1,13 @@
+//
+//  ASStudent.m
+//  BlocksTask
+//
+//  Created by Aleksandr on 29/03/2019.
+//  Copyright © 2019 Aleksandr Shushkov. All rights reserved.
+//
+
+#import "ASStudent.h"
+
+@implementation ASStudent
+
+@end
