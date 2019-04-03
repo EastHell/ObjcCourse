4c48f0b2a17ca4a761c683b54690b01c6e5fc91b
Aleksandr <djturbo9393@gmail.com>
2019-04-04 00:51:17 +0300

    Добавлена задача по строкам

---
+++ StringTask/StringTask/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  StringTask
+//
+//  Created by Aleksandr on 03/04/2019.
+//  Copyright © 2019 Aleksandr Shushkov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
