0e9a42f5df6947370ee66acb6631efef21c84ce1
Aleksandr <djturbo9393@gmail.com>
2019-05-30 22:47:24 +0300

    Исправлены замечания ревью

--- ViewsTask/ViewsTask/ViewController.m
+++ ViewsTask/ViewsTask/ViewController.m
@@ -22,46 +22,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    _fieldView = [[UIView alloc] initWithFrame:CGRectMake(0, 46, MIN(self.view.frame.size.width, self.view.frame.size.height), MIN(self.view.frame.size.width, self.view.frame.size.height))];
-    _fieldView.backgroundColor = [UIColor blueColor];
-    _fieldView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
-    [self.view addSubview:_fieldView];
+    self.fieldView = [[UIView alloc] initWithFrame:CGRectMake(0, 46, MIN(self.view.frame.size.width, self.view.frame.size.height), MIN(self.view.frame.size.width, self.view.frame.size.height))];
+    self.fieldView.backgroundColor = [UIColor blueColor];
+    self.fieldView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
+    [self.view addSubview:self.fieldView];
     
-    const CGFloat SQUAD_SIZE = MIN(_fieldView.frame.size.height, _fieldView.frame.size.width) / 8;
+    const CGFloat SQUAD_SIZE = MIN(self.fieldView.frame.size.height, self.fieldView.frame.size.width) / 8;
     
-    _fieldSquads = [NSMutableArray array];
+    self.fieldSquads = [NSMutableArray array];
     
     for (NSInteger i = 0; i < 32; i++) {
         UIView *view = [[UIView alloc] initWithFrame:CGRectMake(((i / 4) % 2) * SQUAD_SIZE + SQUAD_SIZE * 2 * (i % 4), (i / 4) * SQUAD_SIZE, SQUAD_SIZE, SQUAD_SIZE)];
         view.backgroundColor = [UIColor blackColor];
-        [_fieldView addSubview:view];
-        [_fieldSquads addObject:view];
+        [self.fieldView addSubview:view];
+        [self.fieldSquads addObject:view];
     }
     
-    _checkers = [NSMutableArray array];
+    self.checkers = [NSMutableArray array];
     
     for (NSInteger i = 0; i < 12; i++) {
-        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(SQUAD_SIZE * 0.2 + ((i / 4) % 2) * SQUAD_SIZE + SQUAD_SIZE * 2 * (i % 4), SQUAD_SIZE * 0.2 + (i / 4) * SQUAD_SIZE, SQUAD_SIZE*0.6, SQUAD_SIZE*0.6)];
-        view.backgroundColor = [UIColor whiteColor];
-        [_fieldView addSubview:view];
-        [_checkers addObject:view];
+        UIView *view = [self createCheckerColor:[UIColor whiteColor] AtIndex:i withSize:SQUAD_SIZE];
+        [self.fieldView addSubview:view];
+        [self.checkers addObject:view];
     }
     
     for (NSInteger i = 31; i >= 20; i--) {
-        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(SQUAD_SIZE * 0.2 + ((i / 4) % 2) * SQUAD_SIZE + SQUAD_SIZE * 2 * (i % 4), SQUAD_SIZE * 0.2 + (i / 4) * SQUAD_SIZE, SQUAD_SIZE*0.6, SQUAD_SIZE*0.6)];
-        view.backgroundColor = [UIColor redColor];
-        [_fieldView addSubview:view];
-        [_checkers addObject:view];
+        UIView *view = [self createCheckerColor:[UIColor redColor] AtIndex:i withSize:SQUAD_SIZE];
+        [self.fieldView addSubview:view];
+        [self.checkers addObject:view];
     }
 }
 
-- (void)dealloc
-{
-    <#statements#>
+-(UIView *)createCheckerColor:(UIColor *)color AtIndex:(NSInteger)index withSize:(CGFloat)squadSize {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(squadSize * 0.2 + ((index / 4) % 2) * squadSize + squadSize * 2 * (index % 4), squadSize * 0.2 + (index / 4) * squadSize, squadSize*0.6, squadSize*0.6)];
+    view.backgroundColor = color;
+    return view;
 }
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    for (UIView *view in _fieldSquads) {
+    for (UIView *view in self.fieldSquads) {
         view.backgroundColor = [UIColor greenColor];
     }
 }
