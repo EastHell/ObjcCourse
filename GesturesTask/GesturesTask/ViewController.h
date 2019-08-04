3015419422059f6a944722097b2ff4b63b75ec86
Aleksandr <djturbo9393@gmail.com>
2019-08-04 21:35:52 +0300

    Добавлено задание по Gestures

---
+++ GesturesTask/GesturesTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturesTask
+//
+//  Created by Aleksandr on 01/08/2019.
+//  Copyright © 2019 Aleksandr Shushkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
